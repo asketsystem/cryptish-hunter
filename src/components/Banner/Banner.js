3f4268401617b3e3d65a603b56978e38a52b6bc5
asketsystem <asketsystem1@gmail.com>
2021-12-17 06:43:17 +0000

    Editing func

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Cryptish Hunter
           </Typography>
           <Typography
             variant="subtitle2"
