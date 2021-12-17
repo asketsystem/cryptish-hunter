5850fb8b668496ad903469848dc112c209779215
asketsystem <asketsystem1@gmail.com>
2021-12-17 19:33:12 +0000

    Shifting Coins State

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { createContext, useContext, useEffect, useState } from "react";
 
 const Crypto = createContext();
@@ -5,6 +6,16 @@ const Crypto = createContext();
 const CryptoContext = ({ children }) => {
   const [currency, setCurrency] = useState("EUR");
   const [symbol, setSymbol] = useState("€");
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCoins = async ()  => {
+    setLoading(true);
+    const { data } = await axios.get(CoinList(currency));
+
+    setCoins(data);
+    setLoading(false);
+  };
 
   useEffect(() => {
     if (currency === "EUR") setSymbol("€");
@@ -12,7 +23,7 @@ const CryptoContext = ({ children }) => {
   }, [currency]);
 
   return (
-    <Crypto.Provider value={{ currency, setCurrency, symbol }}>
+    <Crypto.Provider value={{ currency, setCurrency, symbol, coins, loading, fetchCoins }}>
       {children}
     </Crypto.Provider>
   );
