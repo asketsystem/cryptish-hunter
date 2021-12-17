97aa2eb5be0d9a9ae26c7495707f7bd6c9f38baa
asketsystem <asketsystem1@gmail.com>
2021-12-17 06:55:19 +0000

    Adding Func

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import { BrowserRouter,Route } from "react-router-dom"
-import './App.css';
-import Header from './components/Header'
-import Homepage from "./Pages/Homepage";
-import CoinPage from "./Pages/CoinPage"
 import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
-
-  const useStyles = makeStyles(() => ({
-    App: {
-        backgroundColor: "#14161a",
-        color: "white",
-        minhHeight: "100vh",
-    },
-  }));
-
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <BrowserRouter>
       <div className={classes.App}>
-        <Header/>
-        <Route path='/' component={Homepage} exact />
-        <Route path="/coins/:id" component={CoinPage} />
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
       </div>
     </BrowserRouter>
   );
