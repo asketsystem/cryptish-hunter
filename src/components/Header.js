f3cf21634eb116e3a666d93d7b9a878e24dee6d3
asketsystem <asketsystem1@gmail.com>
2021-12-17 19:05:10 +0000

    Update Version

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              Cryptish Hunter
+              Cryptish Tracker
             </Typography>
             {/* <Button color="inherit">Login</Button> */}
             <Select
